bd947743379bf8a8859679025e62e8004d3b2f2b
kimgiraffe <kimgiraffe@naver.com>
2023-11-06 22:04:07 +0900

    feat: add UserDao, userMapper

--- materip/src/main/java/com/ssafy/materip/model/dao/UserDao.java
+++ materip/src/main/java/com/ssafy/materip/model/dao/UserDao.java
@@ -1,8 +1,21 @@
 package com.ssafy.materip.model.dao;
 
+import java.sql.SQLException;
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
+
+import com.ssafy.materip.model.dto.User;
 
 @Mapper
 public interface UserDao {
-
+	int createUser(User user) throws SQLException;
+	
+	User readUserById(String userId) throws SQLException;
+	
+	int updateUser(User user) throws SQLException;
+	
+	int deleteUser(String userId) throws SQLException;
+	
+	List<User> readAllUsers() throws SQLException;
 }
