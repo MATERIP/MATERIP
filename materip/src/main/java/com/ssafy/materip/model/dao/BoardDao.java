866ea15befe8b44705be984d215ddb8fb32b837e
yhw991228 <yhw991228@gmail.com>
2023-11-07 22:04:03 +0900

    feat: add board DAO, Service

--- materip/src/main/java/com/ssafy/materip/model/dao/BoardDao.java
+++ materip/src/main/java/com/ssafy/materip/model/dao/BoardDao.java
@@ -1,8 +1,46 @@
 package com.ssafy.materip.model.dao;
 
+import com.ssafy.materip.model.dto.Board;
+import com.ssafy.materip.model.dto.Comments;
+import com.ssafy.materip.model.dto.Participants;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.jdbc.SQL;
+
+import javax.xml.stream.events.Comment;
+import java.sql.SQLException;
+import java.util.List;
 
 @Mapper
 public interface BoardDao {
+
+    int createBoard(Board board) throws SQLException;
+
+    List<Board> readAllBoards() throws SQLException;
+
+    int updateBoard(Board board) throws  SQLException;
+
+    int deleteBoard(Board board) throws SQLException;
+
+
+    int createComments(Comments comments) throws SQLException;
+
+    List<Comments> readAllComments(Comments comments) throws SQLException;
+
+    int updateComments(Comments comments) throws SQLException;
+
+    int deleteComments(Comments comments) throws SQLException;
+
+
+    int addParticipants(Participants participants) throws SQLException;
+    List<Participants> getParticipants(String board_id) throws SQLException;
+    int removeParticipants(Participants participants) throws SQLException;
+    int getParticipantsCnt(String board_id) throws  SQLException;
+
+
+
+
+
+
+
 
 }
--- materip/src/main/java/com/ssafy/materip/model/service/BoardServiceImpl.java
+++ materip/src/main/java/com/ssafy/materip/model/service/BoardServiceImpl.java
@@ -1,8 +1,73 @@
 package com.ssafy.materip.model.service;
 
+import com.ssafy.materip.model.dao.BoardDao;
+import com.ssafy.materip.model.dto.Board;
+import com.ssafy.materip.model.dto.Comments;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class BoardServiceImpl implements BoardService {
 
+    private final BoardDao boardDao;
+
+    public BoardServiceImpl(BoardDao boardDao) {
+        this.boardDao = boardDao;
+    }
+
+    @Override
+    public int writeBoard(Board board) throws Exception {
+        return 0;
+    }
+
+    @Override
+    public int editBoard(Board board) throws Exception {
+        return 0;
+    }
+
+    @Override
+    public int deleteBoard(Board board) throws Exception {
+        return 0;
+    }
+
+    @Override
+    public List<Board> getBoardList() throws Exception {
+        return null;
+    }
+
+    @Override
+    public int writeComment(Comments comments, String board_id) throws Exception {
+        return 0;
+    }
+
+    @Override
+    public int removeComment(Comments comments) throws Exception {
+        return 0;
+    }
+
+    @Override
+    public int modifyComment(Comments comments) throws Exception {
+        return 0;
+    }
+
+    @Override
+    public List<Comments> getCommentList(String board_id) {
+        return null;
+    }
+
+    @Override
+    public int getCommentCntByBoardId(String board_id) {
+        return 0;
+    }
+
+    @Override
+    public int addParticipants(String board_id, String user_id) {
+        return 0;
+    }
+
+    @Override
+    public int getParticipantsCnt(String board_id) {
+        return 0;
+    }
 }
