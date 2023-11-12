2e6269829d5909a364e18d4bac000ca9b0737424
yhw991228 <yhw991228@gmail.com>
2023-11-12 19:18:19 +0900

    feat: add carousel and modify items for autocomplete

--- materip-vue-project/src/components/NavigationComponent.vue
+++ materip-vue-project/src/components/NavigationComponent.vue
@@ -1,36 +1,35 @@
 <template>
-      <v-app-bar app color="transparent" flat>
-        <v-toolbar-title min-width="10rem">
-          <v-img src="src/assets/orglogo.png" alt="Logo" max-height="32"></v-img>
-        </v-toolbar-title>
-        <v-spacer></v-spacer>
+  <v-app-bar app color="transparent" flat class="px-10">
+    <v-toolbar-title min-width="10rem">
+      <v-img src="src/assets/orglogo.png" alt="Logo" max-height="32"></v-img>
+    </v-toolbar-title>
+    <v-spacer></v-spacer>
 
-        <v-btn text @click="goToReview"><v-icon icon="mdi-note-edit"></v-icon>여행지 리뷰</v-btn>
-        <v-btn text @click="goToMate"><v-icon icon="mdi-account-multiple-plus"></v-icon>여행 메이트</v-btn>
-        <v-autocomplete
-          :items="items"
-          
-          auto-select-first
-          class="flex-full-width align-self-center transparent-background"
-          density="comfortable"
-          item-props
-          menu-icon=''
-          placeholder="search Travel "
-          prepend-inner-icon='mdi-magnify'
-          rounded
-          theme="light"
-          variant="solo"
-          hide-details
-        >
-        </v-autocomplete>
+    <v-btn text @click="goToReview"><v-icon icon="mdi-note-edit"></v-icon>여행지 리뷰</v-btn>
+    <v-btn text @click="goToMate"
+      ><v-icon icon="mdi-account-multiple-plus"></v-icon>여행 메이트</v-btn
+    >
+    <v-autocomplete
+      :items="items"
+      auto-select-first
+      class="flex-full-width align-self-center transparent-background px-7"
+      density="comfortable"
+      item-props
+      menu-icon=""
+      placeholder="search Travel "
+      prepend-inner-icon="mdi-magnify"
+      rounded
+      theme="light"
+      variant="solo"
+      hide-details
+    >
+    </v-autocomplete>
 
-        <v-btn text @click="goToLogin"><v-icon icon="mdi-account"></v-icon>로그인</v-btn>
-      </v-app-bar>
-
-
+    <v-btn text @click="goToLogin" class="mr-10"><v-icon icon="mdi-account"></v-icon>로그인</v-btn>
+  </v-app-bar>
 </template>
 
 <script setup>
-
+import { ref } from 'vue'
+const items = ref(['setup', 'settup', 'mainview'])
 </script>
-
--- materip-vue-project/src/views/MainView.vue
+++ materip-vue-project/src/views/MainView.vue
@@ -1,12 +1,24 @@
-<script setup>
-
+<script>
+export default {
+  data() {
+    return {
+      colors: ['#edfaf7', '#b7efe3', '#9ee4d9', '#85ebbb', '#8cdbec'],
+      slides: ['First', 'Second', 'Third', 'Fourth', 'Fifth']
+    }
+  }
+}
 </script>
 
 <template>
-    <div>
-    </div>
+  <v-carousel cycle height="800" hide-delimiters show-arrows="hover">
+    <v-carousel-item v-for="(slide, i) in slides" :key="i">
+      <v-sheet :color="colors[i]" height="100%">
+        <div class="d-flex fill-height justify-center align-center">
+          <div class="text-h2">{{ slide }} Slide</div>
+        </div>
+      </v-sheet>
+    </v-carousel-item>
+  </v-carousel>
 </template>
 
-<style scoped>
-
-</style>
+<style></style>
