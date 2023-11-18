6de4723dd2860c3420e223b964d6c1fecba67e9e
yhw991228 <yhw991228@gmail.com>
2023-11-18 22:38:57 +0900

    refactor: user store and board store

--- materip-vue-project/src/stores/user-store.js
+++ materip-vue-project/src/stores/user-store.js
@@ -8,57 +8,70 @@ export const useUserStore = defineStore(
   () => {
     // ***************** state *****************
     const axios = inject('axios')
-
+    const userInfo = ref()
+    const userId = ref()
+    const isAdmin = ref(0)
     const menuList = ref([
       { name: '로그인', show: true, routeName: 'login' },
       { name: '마이페이지', show: false, routeName: 'mypage' },
       { name: '로그아웃', show: false, routeName: 'logout' }
     ])
-    const userInfo = ref()
+
+
+    
+
     const changeMenuState = () => {
       menuList.value.forEach((menu) => {
         menu.show = !menu.show
       })
     }
+    const changeUserState = (userid, isadmin) => {
+      userId.value = userid
+      isAdmin.value = isadmin
+    }
 
+    const changeUserInfoState = (userinfo) => { 
+      userInfo.value = userinfo
+    }
     // **************** actions ****************
     const login = async (userInfo) => {
       // 서버로 요청
       console.log(userInfo)
-      await axios.post('/admin/user/login', userInfo).then((response) => {
+      await axios.post('/user/login', userInfo).then((response) => {
         console.log(response)
-        const { accessToken } = response.data
+        const accessToken = response.data["accessToken"]
+        
 
         // 이미 페이지가 로드된 시점에 로그인을 수행 했으므로
         // axios 객체의 아래 값은 초기화가 되어있지 않음으로 값을 저장.
         // accessToken을 storage에 저장하는 경우 취약점이 발생할 수 있다.
         // pinia-plugin-persistedstate 를 사용하는 경우 storage에 저장되는 것을 막아야한다.
         axios.defaults.headers.common['Authorization'] = accessToken
-
+        
         // 로그인을 성공하여 토큰이 정상적으로 저장된 경우
         // 메뉴 표시를 수정.
         alert('로그인!')
         changeMenuState()
+        changeUserState(response.data["userId"], response.data["isAdmin"])
       })
     }
 
     const logout = async () => {
       changeMenuState()
-      await axios.delete('admin/user/logout').then(() => {
+      await axios.delete('/user/logout').then(() => {
         axios.defaults.headers.common['Authorization'] = ''
+        isAdmin.value = 0
+        userId.value = ''
+        userInfo.value = null
         alert('로그아웃!')
       })
     }
 
-    const getUserInfo = async () => {
-      const refreshToken = sessionStorage.getItem('refreshToken')
-      axios.defaults.headers.common['Authorization'] = refreshToken
-
+    const getUserInfo = async (userId) => {
       await axios
-        .get('/admin/user/mypage')
+        .get('/user/' + userId)
         .then((response) => {
-          
-          userInfo.value = response.data["userInfo"]
+          changeUserInfoState(response.data["userInfo"])
         })
         .catch((response) => {
           
@@ -67,7 +80,7 @@ export const useUserStore = defineStore(
 
     // 회원 정보를 수정하면 axios header에 저장된 accessToken을 삭제한다.
     const modify = async (userInfo) => {
-      await axios.put('/admin/user/modify', userInfo).then(() => {
+      await axios.put('/user/modify', userInfo).then(() => {
         axios.defaults.headers.common['Authorization'] = ''
         changeMenuState()
       })
@@ -75,7 +88,7 @@ export const useUserStore = defineStore(
 
     // 회원 탈퇴 시 axios header에 저장된 accessToken을 삭제한다.
     const withdrawal = async (userInfo) => {
-      await axios.delete('admin/user/withdrawal', userInfo).then(() => {
+      await axios.delete('user/withdrawal', userInfo).then(() => {
         axios.defaults.headers.common['Authorization'] = ''
         changeMenuState()
       })
@@ -87,6 +100,8 @@ export const useUserStore = defineStore(
       modify,
       withdrawal,
       userInfo,
+      isAdmin,
+      userId,
       getUserInfo,
       menuList
     }
--- materip/src/main/java/com/ssafy/materip/controller/BoardController.java
+++ materip/src/main/java/com/ssafy/materip/controller/BoardController.java
@@ -99,158 +99,37 @@ public class BoardController {
 			@ApiResponse(code = 404, message = "사용자 정보 없음")
 	})
 	@ResponseBody
-	@AuthRequired
-	@GetMapping("/myboard/review")
-	public ResponseEntity<Map<String, Object>> getReviewBoardbyId(HttpServletRequest request, HttpServletResponse response) throws Exception {
-
+	@GetMapping("/getRecruitmentList/user_id")
+	public ResponseEntity<Map<String, Object>> getReviewBoardbyId(HttpServletRequest request, HttpServletResponse response, @PathVariable("user_id") String userId) throws Exception {
 		Map<String, Object> result = new HashMap<String, Object>();
-	
-		// Authorization에 포함된 accessToken의 userId를 가져온다.
-		String userId = jwtUtil.getUserId(request.getHeader("Authorization"));
-
-		Cookie[] cookieList = request.getCookies();
-
-		String requestRefreshToken = "";
-
-		for(Cookie cookie : cookieList) {
-			if(cookie.getName().equals("refreshToken")) {
-				requestRefreshToken = cookie.getValue();
-			}
-		}
-
-		// 요청으로 들어온 refreshToken이 Database의 refreshToken과 일치하는지 확인.
-		String dbRefreshToken = authService.getRefreshToken(userId).getRefreshToken();
-
-		if(!requestRefreshToken.equals(dbRefreshToken)) {
-			// RefreshToken은 HttpOnly Cookie로 발급
-			Cookie cookie = new Cookie("refreshToken", "");
-			cookie.setMaxAge(0);
-			cookie.setHttpOnly(true);
-			cookie.setPath("/");
-			System.out.println("토큰이 다름");
-			response.addCookie(cookie);
-
-			result.put("message", "로그인이 필요합니다.");
-			return new ResponseEntity<Map<String, Object>>(result, HttpStatus.FORBIDDEN);
-		}
 
 		// 해당 userId의 정보를 획득.
 		
 		List<Board> boardList = boardService.getReviewListById(userId);
-
-		
-
 		result.put("boardList", boardList);
 		return new ResponseEntity<Map<String, Object>>(result, HttpStatus.OK);
 	}
 
 
 
-	@ApiOperation(value="사용자별 메이트 보드 얻기", notes="접속한 사용자의 보드들을 얻어옵니다.")
+
+	@ApiOperation(value="사용자별 모집 정보 얻기", notes="접속한 사용자의 메이트 모집글들을 얻어옵니다.")
 	@ApiResponses({
 			@ApiResponse(code = 200, message = "보드 정보 반환 성공"),
 			@ApiResponse(code = 403, message = "권한 없음"),
 			@ApiResponse(code = 404, message = "사용자 정보 없음")
 	})
 	@ResponseBody
-	@AuthRequired
-	@GetMapping("/myboard/mate")
-	public ResponseEntity<Map<String, Object>> getMateBoardbyId(HttpServletRequest request, HttpServletResponse response) throws Exception {
-
+	@GetMapping("/getRecruitmentList/user_id")
+	public ResponseEntity<Map<String, Object>> getMateBoardbyId(HttpServletRequest request, HttpServletResponse response, @PathVariable("user_id") String userId) throws Exception {
 		Map<String, Object> result = new HashMap<String, Object>();
-
-		// Authorization에 포함된 accessToken의 userId를 가져온다.
-		String userId = jwtUtil.getUserId(request.getHeader("Authorization"));
-
-		Cookie[] cookieList = request.getCookies();
-
-		String requestRefreshToken = "";
-
-		for(Cookie cookie : cookieList) {
-			if(cookie.getName().equals("refreshToken")) {
-				requestRefreshToken = cookie.getValue();
-			}
-		}
-
-		// 요청으로 들어온 refreshToken이 Database의 refreshToken과 일치하는지 확인.
-		String dbRefreshToken = authService.getRefreshToken(userId).getRefreshToken();
-
-		if(!requestRefreshToken.equals(dbRefreshToken)) {
-			// RefreshToken은 HttpOnly Cookie로 발급
-			Cookie cookie = new Cookie("refreshToken", "");
-			cookie.setMaxAge(0);
-			cookie.setHttpOnly(true);
-			cookie.setPath("/");
-			System.out.println("토큰이 다름");
-			response.addCookie(cookie);
-
-			result.put("message", "로그인이 필요합니다.");
-			return new ResponseEntity<Map<String, Object>>(result, HttpStatus.FORBIDDEN);
-		}
 
 		// 해당 userId의 정보를 획득.
 
 		List<Board> boardList = boardService.getMateListById(userId);
-
 		result.put("boardList", boardList);
 		return new ResponseEntity<Map<String, Object>>(result, HttpStatus.OK);
 	}
-
-
-
-
-
-
-
-	@ApiOperation(value="관리자 여부", notes="관리자 여부인지를 반환합니다.")
-	@ApiResponses({
-			@ApiResponse(code = 200, message = "사용자 정보 반환 성공"),
-			@ApiResponse(code = 403, message = "권한 없음"),
-			@ApiResponse(code = 404, message = "사용자 정보 없음")
-	})
-	@ResponseBody
-	@GetMapping("/modifyAuth")
-	// board list => list중에 하나 클릭하면 => boardid 넘어가고 => boardid넘어가면 boardid board detail + modify?
-	public ResponseEntity<Map<String, Object>> modifyAuth(HttpServletRequest request, HttpServletResponse response) throws Exception {
-
-		Map<String, Object> result = new HashMap<String, Object>();
-		// Authorization에 포함된 accessToken의 userId를 가져온다.
-		String userId = request.getHeader("Authorization").equals("")? "":jwtUtil.getUserId(request.getHeader("Authorization"));
-
-		Cookie[] cookieList = request.getCookies();
-
-		String requestRefreshToken = "";
-
-		for(Cookie cookie : cookieList) {
-			if(cookie.getName().equals("refreshToken")) {
-				requestRefreshToken = cookie.getValue();
-			}
-		}
-		System.out.println(requestRefreshToken);
-		// 요청으로 들어온 refreshToken이 Database의 refreshToken과 일치하는지 확인.
-		String dbRefreshToken = authService.getRefreshToken(userId).getRefreshToken();
-
-		if(!requestRefreshToken.equals(dbRefreshToken)) {
-			// RefreshToken은 HttpOnly Cookie로 발급
-			Cookie cookie = new Cookie("refreshToken", "");
-			cookie.setMaxAge(0);
-			cookie.setHttpOnly(true);
-			cookie.setPath("/");
-			System.out.println("토큰이 다름");
-			response.addCookie(cookie);
-
-			result.put("message", "로그인이 필요합니다.");
-			return new ResponseEntity<Map<String, Object>>(result, HttpStatus.FORBIDDEN);
-		}
-
-		int boardId = Integer.parseInt(request.getParameter("board-id"));
-		Board board = boardService.getBoardById(boardId);
-		result.put("boardDetail", board);
-		result.put("auth",board.author.equals(userId));
-
-		return new ResponseEntity<Map<String, Object>>(result, HttpStatus.OK);
-	}
-
 
 
 
@@ -282,12 +161,23 @@ public class BoardController {
 		return new ResponseEntity<>(result, HttpStatus.OK);
 	}
 
+
+	@ApiOperation(value = "조회수 증가", notes = "게시판 조회수 증가")
+	@GetMapping("/updateHits/{board_id}")
+	public ResponseEntity<?> updateHits(@PathVariable("board_id") int board_id) throws Exception {
+		boardService.updateBoardHits(board_id);
+		return new ResponseEntity<>(HttpStatus.OK);
+	}
+
+
 	@ApiOperation(value = "댓글 리스트", notes = "보드 아이디에 따라 댓글 리스트를 불러옵니다", response = Comments.class)
 	@GetMapping("/comment/getList/{board_id}")
 	public ResponseEntity<?> getCommentList(@PathVariable("board_id") int board_id) throws Exception {
 		List<Comments> result = boardService.getCommentList(board_id);
 		return new ResponseEntity<>(result, HttpStatus.OK);
 	}
+
+
 
 	@ApiOperation(value = "댓글 개수", notes = "보드 아이디에 따른 댓글 개수를 불러옵니다.", response = Integer.class)
 	@GetMapping("/comment/getCommentCnt/{board_id}")
@@ -296,12 +186,16 @@ public class BoardController {
 		return new ResponseEntity<>(result, HttpStatus.OK);
 	}
 
+
+
 	@ApiOperation(value = "댓글 작성", notes = "댓글 작성합니다. ", response = Integer.class)
 	@PostMapping("/comment/write")
 	public ResponseEntity<?> writeComment(@RequestBody Comments comments, int board_id) throws Exception {
 		int result = boardService.writeComment(comments, board_id);
 		return new ResponseEntity<>(result, HttpStatus.OK);
 	}
+
+
 
 	@ApiOperation(value = "댓글 수정", notes = "댓글 수정합니다.", response = Integer.class)
 	@PostMapping("/comment/modify")
@@ -310,18 +204,15 @@ public class BoardController {
 		return new ResponseEntity<>(result, HttpStatus.OK);
 	}
 
+
+
+
 	@ApiOperation(value = "댓글 삭제", notes = "댓글을 삭제합니다.", response = Integer.class)
 	@DeleteMapping("/comment/delete/{sequence}")
 	public ResponseEntity<?> deleteComment(@PathVariable("sequence") int sequence) throws Exception {
 		int result = boardService.removeComment(sequence);
 		return new ResponseEntity<>(result, HttpStatus.OK);
 	}
-	
-	@ApiOperation(value = "조회수 증가", notes = "게시판 조회수 증가")
-	@GetMapping("/updateHits/{board_id}")
-	public ResponseEntity<?> updateHits(@PathVariable("board_id") int board_id) throws Exception {
-		boardService.updateBoardHits(board_id);
-		return new ResponseEntity<>(HttpStatus.OK);
-	}
+
 
 }
